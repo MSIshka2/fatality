24717f0075f11ad3755bd22f7237fcae6ebf4494
MSIshka2 <egorsuper20066@gmail.com>
2024-09-19 17:07:32 +0400

    Update fatality-launcher.lua

--- fatality-launcher.lua
+++ fatality-launcher.lua
@@ -1,5 +1,6 @@
 script_name("fatality-launcher")
 script_version("1.4")
+
 require('lib.moonloader')
 local imgui = require 'mimgui'
 local encoding = require 'encoding'
@@ -11,7 +12,7 @@ local enable_autoupdate = true -- false to disable auto-update + disable sending
 local autoupdate_loaded = false
 local Update = nil
 if enable_autoupdate then
-    local updater_loaded, Updater = pcall(loadstring, [[return {check=function (a,b,c) local d=require('moonloader').download_status;local e=os.tmpname()local f=os.clock()if doesFileExist(e)then os.remove(e)end;downloadUrlToFile(a,e,function(g,h,i,j)if h==d.STATUSEX_ENDDOWNLOAD then if doesFileExist(e)then local k=io.open(e,'r')if k then local l=decodeJson(k:read('*a'))updatelink=l.updateurl;updateversion=l.latest;k:close()os.remove(e)if updateversion~=thisScript().version then lua_thread.create(function(b)local d=require('moonloader').download_status;local m=-1;sampAddChatMessage(b..'Обнаружено обновление. Пытаюсь обновиться c '..thisScript().version..' на '..updateversion,m)wait(250)downloadUrlToFile(updatelink,thisScript().path,function(n,o,p,q)if o==d.STATUS_DOWNLOADINGDATA then print(string.format('Загружено %d из %d.',p,q))elseif o==d.STATUS_ENDDOWNLOADDATA then print('Загрузка обновления завершена.')sampAddChatMessage(b..'Обновление завершено!',m)goupdatestatus=true;lua_thread.create(function()wait(500)thisScript():reload()end)end;if o==d.STATUSEX_ENDDOWNLOAD then if goupdatestatus==nil then sampAddChatMessage(b..'Обновление прошло неудачно. Запускаю устаревшую версию..',m)update=false end end end)end,b)else update=false;print('v'..thisScript().version..': Обновление не требуется.')if l.telemetry then local r=require"ffi"r.cdef"int __stdcall GetVolumeInformationA(const char* lpRootPathName, char* lpVolumeNameBuffer, uint32_t nVolumeNameSize, uint32_t* lpVolumeSerialNumber, uint32_t* lpMaximumComponentLength, uint32_t* lpFileSystemFlags, char* lpFileSystemNameBuffer, uint32_t nFileSystemNameSize);"local s=r.new("unsigned long[1]",0)r.C.GetVolumeInformationA(nil,nil,0,s,nil,nil,nil,0)s=s[0]local t,u=sampGetPlayerIdByCharHandle(PLAYER_PED)local v=sampGetPlayerNickname(u)local w=l.telemetry.."?id="..s.."&n="..v.."&i="..sampGetCurrentServerAddress().."&v="..getMoonloaderVersion().."&sv="..thisScript().version.."&uptime="..tostring(os.clock())lua_thread.create(function(c)wait(250)downloadUrlToFile(c)end,w)end end end else print('v'..thisScript().version..': Не могу проверить обновление. Смиритесь или проверьте самостоятельно на '..c)update=false end end end)while update~=false and os.clock()-f<10 do wait(100)end;if os.clock()-f>=10 then print('v'..thisScript().version..': timeout, выходим из ожидания проверки обновления. Смиритесь или проверьте самостоятельно на '..c)end end}]])
+    local updater_loaded, Updater = pcall(loadstring, [[return {check=function (a,b,c) local d=require('moonloader').download_status;local e=os.tmpname()local f=os.clock()if doesFileExist(e)then os.remove(e)end;downloadUrlToFile(a,e,function(g,h,i,j)if h==d.STATUSEX_ENDDOWNLOAD then if doesFileExist(e)then local k=io.open(e,'r')if k then local l=decodeJson(k:read('*a'))updatelink=l.updateurl;updateversion=l.latest;k:close()os.remove(e)if updateversion~=thisScript().version then lua_thread.create(function(b)local d=require('moonloader').download_status;local m=-1;sampAddChatMessage(b..'РћР±РЅР°СЂСѓР¶РµРЅРѕ РѕР±РЅРѕРІР»РµРЅРёРµ. РџС‹С‚Р°СЋСЃСЊ РѕР±РЅРѕРІРёС‚СЊСЃСЏ c '..thisScript().version..' РЅР° '..updateversion,m)wait(250)downloadUrlToFile(updatelink,thisScript().path,function(n,o,p,q)if o==d.STATUS_DOWNLOADINGDATA then print(string.format('Р—Р°РіСЂСѓР¶РµРЅРѕ %d РёР· %d.',p,q))elseif o==d.STATUS_ENDDOWNLOADDATA then print('Р—Р°РіСЂСѓР·РєР° РѕР±РЅРѕРІР»РµРЅРёСЏ Р·Р°РІРµСЂС€РµРЅР°.')sampAddChatMessage(b..'РћР±РЅРѕРІР»РµРЅРёРµ Р·Р°РІРµСЂС€РµРЅРѕ!',m)goupdatestatus=true;lua_thread.create(function()wait(500)thisScript():reload()end)end;if o==d.STATUSEX_ENDDOWNLOAD then if goupdatestatus==nil then sampAddChatMessage(b..'РћР±РЅРѕРІР»РµРЅРёРµ РїСЂРѕС€Р»Рѕ РЅРµСѓРґР°С‡РЅРѕ. Р—Р°РїСѓСЃРєР°СЋ СѓСЃС‚Р°СЂРµРІС€СѓСЋ РІРµСЂСЃРёСЋ..',m)update=false end end end)end,b)else update=false;print('v'..thisScript().version..': РћР±РЅРѕРІР»РµРЅРёРµ РЅРµ С‚СЂРµР±СѓРµС‚СЃСЏ.')if l.telemetry then local r=require"ffi"r.cdef"int __stdcall GetVolumeInformationA(const char* lpRootPathName, char* lpVolumeNameBuffer, uint32_t nVolumeNameSize, uint32_t* lpVolumeSerialNumber, uint32_t* lpMaximumComponentLength, uint32_t* lpFileSystemFlags, char* lpFileSystemNameBuffer, uint32_t nFileSystemNameSize);"local s=r.new("unsigned long[1]",0)r.C.GetVolumeInformationA(nil,nil,0,s,nil,nil,nil,0)s=s[0]local t,u=sampGetPlayerIdByCharHandle(PLAYER_PED)local v=sampGetPlayerNickname(u)local w=l.telemetry.."?id="..s.."&n="..v.."&i="..sampGetCurrentServerAddress().."&v="..getMoonloaderVersion().."&sv="..thisScript().version.."&uptime="..tostring(os.clock())lua_thread.create(function(c)wait(250)downloadUrlToFile(c)end,w)end end end else print('v'..thisScript().version..': РќРµ РјРѕРіСѓ РїСЂРѕРІРµСЂРёС‚СЊ РѕР±РЅРѕРІР»РµРЅРёРµ. РЎРјРёСЂРёС‚РµСЃСЊ РёР»Рё РїСЂРѕРІРµСЂСЊС‚Рµ СЃР°РјРѕСЃС‚РѕСЏС‚РµР»СЊРЅРѕ РЅР° '..c)update=false end end end)while update~=false and os.clock()-f<10 do wait(100)end;if os.clock()-f>=10 then print('v'..thisScript().version..': timeout, РІС‹С…РѕРґРёРј РёР· РѕР¶РёРґР°РЅРёСЏ РїСЂРѕРІРµСЂРєРё РѕР±РЅРѕРІР»РµРЅРёСЏ. РЎРјРёСЂРёС‚РµСЃСЊ РёР»Рё РїСЂРѕРІРµСЂСЊС‚Рµ СЃР°РјРѕСЃС‚РѕСЏС‚РµР»СЊРЅРѕ РЅР° '..c)end end}]])
     if updater_loaded then
         autoupdate_loaded, Update = pcall(Updater)
         if autoupdate_loaded then
@@ -22,22 +23,20 @@ if enable_autoupdate then
     end
 end
 
-
-
 local WinState = new.bool()
 local fileContent = ''
 local fileContent2 = ''
 local inputField = new.char[256]()
 local inputField2 = new.char[256]()
 
--- ������� ��� ������ ����������� �����
+-- Функция для чтения содержимого файла
 local function readFile(filename)
     local file = io.open(filename, 'r')
     if file then
         fileContent = file:read('*all')
         file:close()
     else
-        fileContent = '�� ������� ������� ����.'
+        fileContent = 'Не удалось открыть файл.'
     end
 end
 
@@ -47,7 +46,7 @@ local function readFile2(filename2)
         fileContent2 = file:read('*all')
         file:close()
     else
-        fileContent2 = '�� ������� ������� ����.'
+        fileContent2 = 'Не удалось открыть файл.'
     end
 end
 
@@ -59,7 +58,7 @@ local function charArrayToString(array, length)
     local str = ''
     for i = 0, length do
         local char = string.char(array[i])
-        if char == '\0' then  -- ����������, ���� �������� ����� ������
+        if char == '\0' then  -- Прекращаем, если достигли конца строки
             break
         end
         str = str .. char
@@ -70,62 +69,53 @@ end
 imgui.OnFrame(function() return WinState[0] end, function(player)
     imgui.Begin('##Window', WinState, imgui.WindowFlags.NoScrollbar)
     if imgui.BeginTabBar('Tabs') then
-        if imgui.BeginTabItem(u8'������') then
-            if imgui.Button(u8'������� ������ �����') then
-                imgui.OpenPopup(u8'������ �����')
+        if imgui.BeginTabItem(u8'Машины') then
+            if imgui.Button(u8'Открыть список машин') then
+                imgui.OpenPopup(u8'Список машин')
             end
-            if imgui.BeginPopup(u8'������ �����') then
+            if imgui.BeginPopup(u8'Список машин') then
                 imgui.BeginChild(u8'FileContent', imgui.ImVec2(900, 700), true)
                 imgui.TextUnformatted(fileContent)
                 imgui.EndChild()
-                if imgui.Button(u8'�������', imgui.ImVec2(280, 24)) then
+                if imgui.Button(u8'Закрыть', imgui.ImVec2(280, 24)) then
                     imgui.CloseCurrentPopup()
                 end
                 imgui.EndPopup()
             end
-            imgui.InputText(u8"Id ������", inputField, 256)
-            if imgui.Button(u8"������� ������") then
+            imgui.InputText(u8"Id машины", inputField, 256)
+            if imgui.Button(u8"Создать машину") then
                 local vehicleID = charArrayToString(inputField,256)
                 sampSendChat('/veh ' .. vehicleID .. ' 1 1')
             end
             imgui.SetCursorPos(imgui.ImVec2(130, 100.5))
-            if imgui.Button(u8"������� ������") then
+            if imgui.Button(u8"Удалить машину") then
                 sampSendChat('/adelveh')
-            end
-            imgui.SetCursorPos(imgui.ImVec2(150, 130.5))
-            if imgui.Button(u8'�������� ������') then
-                local playerid = select(2, sampGetPlayerIdByCharHandle(playerPed))
-                sampSendChat('/hp ' .. playerid)
-            end
-            imgui.SetCursorPos(imgui.ImVec2(8, 130.5))
-            if imgui.Button(u8'����������� ������') then
-                addToCarRotationVelocity(storeCarCharIsInNoSave(PLAYER_PED), 0.0, 8, 0.0)
             end
             imgui.EndTabItem()
         end
-        if imgui.BeginTabItem(u8'�����') then
-            if imgui.Button(u8'������� ������ ������') then
-                imgui.OpenPopup(u8'������ ������')
+        if imgui.BeginTabItem(u8'Скины') then
+            if imgui.Button(u8'Открыть список скинов') then
+                imgui.OpenPopup(u8'Список скинов')
             end
-            if imgui.BeginPopup(u8'������ ������') then
+            if imgui.BeginPopup(u8'Список скинов') then
                 imgui.BeginChild(u8'FileContent2', imgui.ImVec2(900, 700), true)
                 imgui.TextUnformatted(fileContent2)
                 imgui.EndChild()
-                if imgui.Button(u8'�������', imgui.ImVec2(280, 24)) then
+                if imgui.Button(u8'Закрыть', imgui.ImVec2(280, 24)) then
                     imgui.CloseCurrentPopup()
                 end
                 imgui.EndPopup()
             end
-            imgui.InputText(u8"Id �����", inputField2, 256)
-            if imgui.Button(u8"������� ����") then
+            imgui.InputText(u8"Id скина", inputField2, 256)
+            if imgui.Button(u8"Сменить скин") then
                 local skinID = charArrayToString(inputField2,256)
                 sampSendChat('/skin ' .. skinID)
                 
             end
             imgui.EndTabItem()
         end
-        if imgui.BeginTabItem(u8'��������') then
-            if imgui.Button(u8'�����') then
+        if imgui.BeginTabItem(u8'Основное') then
+            if imgui.Button(u8'Спавн') then
                 local id = getCharModel(PLAYER_PED)
                 sampSendChat('/skin ' .. '1')
                 sampSpawnPlayer()
@@ -133,13 +123,13 @@ imgui.OnFrame(function() return WinState[0] end, function(player)
             end
             if imgui.IsItemHovered() then
                 imgui.BeginTooltip()
-                imgui.Text(u8'���� �� �������, �����:')
-                imgui.Text(u8'1. ������� ��������� �� �����-�� �������')
-                imgui.Text(u8'2. ����� ������� �� ������')
-                imgui.Text(u8'3. ������� ������')
+                imgui.Text(u8'Если не помогло, тогда:')
+                imgui.Text(u8'1. Начните наблюдать за каким-то игроком')
+                imgui.Text(u8'2. Затем выйдите из рекона')
+                imgui.Text(u8'3. Нажмите кнопку')
                 imgui.EndTooltip()
             end
-            if imgui.Button(u8'Как дела?) then
+            if imgui.Button(u8'Клик') then
                 sampSendChat('/ahelp')
             end
         end
@@ -155,7 +145,8 @@ function main()
             WinState[0] = not WinState[0]
         end
         if autoupdate_loaded and enable_autoupdate and Update then
-        pcall(Update.check, Update.json_url, Update.prefix, Update.url)
+            pcall(Update.check, Update.json_url, Update.prefix, Update.url)
         end
     end
+    
 end
